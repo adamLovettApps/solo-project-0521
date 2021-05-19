0b1f374b30106976f81453460796bde3f25e5fa2
Adam Lovett <adamlovett@alumni.usc.edu>
2021-05-19 15:26:16 -0700

    finish signup validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,14 +1,26 @@
 // backend/routes/api/session.js
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-
+const { check } = require('express-validator');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
+const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
+
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors,
+];
 
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
         const { credential, password } = req.body;
 
@@ -36,6 +48,20 @@ router.delete(
     (_req, res) => {
         res.clearCookie('token');
         return res.json({ message: 'success' });
+    }
+);
+
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
     }
 );
 
