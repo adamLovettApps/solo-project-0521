23fb2334359c007ced99e6a3be0090c5bc6b102d
Adam Lovett <adamlovett@alumni.usc.edu>
2021-05-19 14:19:06 -0700

    first commit

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const { environment } = require('./config');
+const routes = require('./routes');
+
+let isProduction = environment === 'production';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+if (!isProduction) {
+    app.use(cors());
+};
+
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
+
+app.use(csurf({
+    cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true,
+    }
+}));
+
+app.use(routes);
+
+module.exports = app;
