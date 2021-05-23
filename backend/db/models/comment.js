01ed1c1bf80356e08818dbd470409896b8c7ef81
Adam Lovett <adamlovett@alumni.usc.edu>
2021-05-22 19:44:08 -0700

    complete db setup

--- backend/db/models/comment.js
+++ backend/db/models/comment.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
   Comment.associate = function(models) {
     // associations can be defined here
     Comment.belongsTo(models.Photo, {foreignKey: "photoId"});
-    Comment.belondsTo(models.User, { foreignKey: "userId"});
+    Comment.belongsTo(models.User, { foreignKey: "userId"});
   };
   return Comment;
 };
